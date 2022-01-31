b98f8bc227df36fc99b59d772ca1600b14aa55f8
CodeHz <codehz@outlook.com>
2022-02-01 00:52:30 +0800

    simplify types

--- types.ts
+++ types.ts
@@ -1,4 +1,7 @@
 export type Payload = {
   game: string;
   user_id: number;
-} & ({ inline_message_id: string, chat_id: undefined; message_id: undefined } | { inline_message_id: undefined, chat_id: number; message_id: number });
+  inline_message_id?: string;
+  chat_id?: number;
+  message_id?: number;
+};
